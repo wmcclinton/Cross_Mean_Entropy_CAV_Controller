35d0a12951315070bc74fc3c7c330a4722f3157e
Me <me@Mes-MacBook-Pro-2.local>
2019-09-28 18:02:33 -0400

    Full update

--- weekend_sprint_v2.py
+++ weekend_sprint_v2.py
@@ -78,7 +78,7 @@ processor = AtariProcessor()
 
 # Finally, we configure and compile our agent. You can use every built-in Keras optimizer and
 # even the metrics!
-memory = EpisodeParameterMemory(limit=10000, window_length=WINDOW_LENGTH)
+memory = EpisodeParameterMemory(limit=100000, window_length=WINDOW_LENGTH)
 
 cem = CEMAgent(model=model, nb_actions=nb_actions, memory=memory, processor=processor,
                batch_size=50, nb_steps_warmup=2000, train_interval=50, elite_frac=0.05)
@@ -87,7 +87,7 @@ cem.compile()
 # Okay, now it's time to learn something! We visualize the training here for show, but this
 # slows down training quite a lot. You can always safely abort the training prematurely using
 # Ctrl + C.
-cem.fit(env, nb_steps=100000, visualize=False, verbose=2)
+cem.fit(env, nb_steps=1000000, visualize=False, verbose=2)
 
 # After training is done, we save the best weights.
 cem.save_weights('cem_{}_params.h5f'.format(ENV_NAME), overwrite=True)
