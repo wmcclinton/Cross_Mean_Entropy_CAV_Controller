106a5aaecbd4da061688ec926f50db0c632f013f
Me <me@Mes-MacBook-Pro-2.local>
2019-09-28 18:01:52 -0400

    Full update

--- weekend_sprint_v2.py
+++ weekend_sprint_v2.py
@@ -28,6 +28,13 @@ from rl.memory import SequentialMemory
 from rl.core import Processor
 from rl.callbacks import TrainEpisodeLogger, ModelIntervalCheckpoint
 
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten, Softmax
+from keras.optimizers import Adam
+
+from rl.agents.cem import CEMAgent
+from rl.memory import EpisodeParameterMemory
+
 #We downsize the atari frame to 84 x 84 and feed the model 4 frames at a time for
 #a sense of direction and speed.
 INPUT_SHAPE = (env.observation_space.shape)
@@ -63,57 +70,27 @@ frame = Input(shape=(input_shape))
 dense = Dense(512, activation='relu')(frame)
 dense = Dense(512, activation='relu')(dense)
 buttons = Dense(nb_actions, activation='linear')(dense)
+buttons = Softmax()(buttons)
 model = Model(inputs=frame,outputs=buttons)
 print(model.summary())
 
-memory = SequentialMemory(limit=1000000, window_length=WINDOW_LENGTH)
-
 processor = AtariProcessor()
 
-policy = LinearAnnealedPolicy(EpsGreedyQPolicy(), attr='eps', value_max=1., value_min=.1, value_test=.05, nb_steps=1250000)
+# Finally, we configure and compile our agent. You can use every built-in Keras optimizer and
+# even the metrics!
+memory = EpisodeParameterMemory(limit=10000, window_length=WINDOW_LENGTH)
 
-dqn = DQNAgent(model=model, nb_actions=nb_actions, policy=policy, memory=memory,
-               processor=processor, enable_double_dqn=False, enable_dueling_network=False, nb_steps_warmup=50000, gamma=.99, target_model_update=10000,
-               train_interval=4, delta_clip=1.)
+cem = CEMAgent(model=model, nb_actions=nb_actions, memory=memory, processor=processor,
+               batch_size=50, nb_steps_warmup=2000, train_interval=50, elite_frac=0.05)
+cem.compile()
 
-dqn.compile(Adam(lr=.00025), metrics=['mae'])
+# Okay, now it's time to learn something! We visualize the training here for show, but this
+# slows down training quite a lot. You can always safely abort the training prematurely using
+# Ctrl + C.
+cem.fit(env, nb_steps=100000, visualize=False, verbose=2)
 
-folder_path = '../model_saves/Vanilla/'
+# After training is done, we save the best weights.
+cem.save_weights('cem_{}_params.h5f'.format(ENV_NAME), overwrite=True)
 
-train = True
-
-if train:
-    weights_filename = folder_path + 'dqn_{}_weights.h5f'.format("CAV_Controller")
-    checkpoint_weights_filename = folder_path + 'dqn_' + "CAV_Controller" + '_weights_{step}.h5f'
-    log_filename = folder_path + 'dqn_' + "CAV_Controller" + '_REWARD_DATA.txt'
-    callbacks = [ModelIntervalCheckpoint(checkpoint_weights_filename, interval=500000)]
-    #callbacks += [TrainEpisodeLogger(log_filename)]
-    #dqn.fit(env, callbacks=callbacks, nb_steps=10000000, verbose=6, nb_max_episode_steps=20000)
-    dqn.fit(env, nb_steps=10000000, verbose=6, nb_max_episode_steps=20000)
-
-else:
-    weights_filename = folder_path + 'dqn_MsPacmanDeterministic-v4_weights_10000000.h5f'
-    if args.weights:
-        weights_filename = args.weights
-    dqn.load_weights(weights_filename)
-    dqn.test(env, nb_episodes=10, visualize=True, nb_max_start_steps=80)
-
-quit()
-
-num_episodes = 100
-rewards = []
-
-for i in range(num_episodes):
-    s = env.reset()
-
-    done = 0
-    i = 0
-    while not done:
-        #env.render()
-        v, x, a = env.CACC(s,env.num_leading_cars)
-        print(a)
-        s, reward, done, info = env.step(a,controller="CACC")
-        i = i + 1
-
-    print(reward)
-    quit()
+# Finally, evaluate our algorithm for 5 episodes.
+cem.test(env, nb_episodes=5, visualize=True)
